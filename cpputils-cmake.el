47126e9a1216bdf53de01bb612135424ba452d56
chen bin <chenbin.sh@gmail.com>
2012-12-26 10:54:38 +0800

    give hint when build directory is missing

--- cpputils-cmake.el
+++ cpputils-cmake.el
@@ -4,7 +4,7 @@
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/cpputils-cmake
 ;; Keywords: CMake IntelliSense Flymake
-;; Version: 0.1.0
+;; Version: 0.1.1
 
 ;; This file is not part of GNU Emacs.
 
@@ -320,9 +320,12 @@ White space here is any of: space, tab, emacs newline (line feed, ASCII 10)."
         bd
         )
     ;; (clrhash cppcm-hash) ; if we open a cmake and non-cmake project ...
-    (when (cppcm-get-dirs)
-      (cppcm-create-flymake-makefiles cppcm-src-dir cppcm-src-dir cppcm-build-dir)
-      (cppcm-set-cxxflags-current-buffer)
+    (if (cppcm-get-dirs)
+        (progn
+          (cppcm-create-flymake-makefiles cppcm-src-dir cppcm-src-dir cppcm-build-dir)
+          (cppcm-set-cxxflags-current-buffer)
+          )
+      (message "Build directory is missing! Create the directory. Then run cmake and make in it.")
       )
     )
   )
