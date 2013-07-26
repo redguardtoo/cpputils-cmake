32012f3d02a97e9dbb0aac318c625fac1eca6f5d
Chen Bin <Chen.Bin@optus.com.au>
2013-07-26 16:32:36 +1000

    updated doc and version is 0.3.4 now

--- cpputils-cmake-pkg.el
+++ cpputils-cmake-pkg.el
@@ -1,2 +1,2 @@
-(define-package "cpputils-cmake" "0.3.3"
+(define-package "cpputils-cmake" "0.3.4"
                 "Easy real time C++ syntax check and intellisense if you use CMake.")
--- pkg.sh
+++ pkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #pkg=cpputils-cmake-`date +%Y%m%d.%H%M`
-pkg=cpputils-cmake-0.3.3
+pkg=cpputils-cmake-0.3.4
 mkdir $pkg
 cp README.org $pkg
 cp *.el $pkg
