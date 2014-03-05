9f2e5f4e5b2cda8c7c4f9f13f20fef0d69d1ff86
Chen Bin <chenbin.sh@gmail.com>
2014-03-05 22:26:26 +1100

    add credits v0.4.6

--- cpputils-cmake.el
+++ cpputils-cmake.el
@@ -4,7 +4,7 @@
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
 ;; URL: http://github.com/redguardtoo/cpputils-cmake
 ;; Keywords: CMake IntelliSense Flymake
-;; Version: 0.4.5
+;; Version: 0.4.6
 
 ;; This file is not part of GNU Emacs.
 
